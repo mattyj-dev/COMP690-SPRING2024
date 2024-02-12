f7a7d3c13982ee39f885dbcde1886b82b395b576
mattyj-dev <mjuleson@gmail.com>
2024-02-12 13:57:17 -0800

    innitial commit

---
+++ Lab 01 - YourFirstJavaScriptApplication/script.js
@@ -0,0 +1,16 @@
+/*
+    Project: Lab 01: My First JavaScript Application
+    Date: 2/9/24
+    Author: Matthew Juleson
+*/
+
+// Declare the variable and collect a number from the user
+let num = prompt('Guess a number between 1 and 10:');
+// Check and see if they entered the correct number
+if (num == 5) {
+    // Display message saying they won
+    alert('You guessed the right number!');
+} else {
+    // Display message saying they lost
+    alert('You guessed the wrong number!');
+}
