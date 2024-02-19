c015216fc589ea20382cd42a8c575b36e5fefbb4
mattyj-dev <mjuleson@gmail.com>
2024-02-19 15:55:32 -0800

    finish step 4 assignment01

--- Assignment01/js/script.js
+++ Assignment01/js/script.js
@@ -1,3 +1,27 @@
-// STEP 1
-// STEP 2
-// STEP 3
+// STEP 1 Convert the following highlighted identifiers to Camel Case notation:
+// let someMonth
+// function theMonth()
+// let currentMonth
+// let summerMonth
+// let myLibraryFunction
+
+// STEP 2 Give me an example of:
+//  a numeric literal expression: 58008
+//  a string literal expression: "Hi, Zak!"
+//  a Boolean literal expression: true
+//  a null literal expression: null
+
+// STEP 3 Give me two examples of:
+//  let complexExpression = 5 * (5/5) - 5
+//  let variableExpression = 58008
+
+// STEP 4 Declare (but do not assign) 9 variables for the following identifiers [Use Camel Casing and Hungarian Notation when naming your identifiers]:
+// let strFirstName
+// let strLastName
+// let strAddress
+// let strCity
+// let strState
+// let intZipCode
+// let intYourAge
+// let strReferralSource
+// let boolMayWeContactYou
